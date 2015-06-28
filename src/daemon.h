8ce99ba1aad2bf5c4e889f54f8b25ffac72241a6
Felix Palmen <felix@palmen-it.de>
2015-06-28 11:15:56 +0200

    daemon.h: GNU-C printf-attributes

--- src/daemon.h
+++ src/daemon.h
@@ -24,8 +24,10 @@ extern void daemon_init(const char *name);
 extern int daemon_daemonize(const daemon_loop daemon_main, void *data);
 extern void daemon_print(const char *message);
 extern void daemon_print_level(int level, const char *message);
-extern void daemon_printf(const char *message_fmt, ...);
-extern void daemon_printf_level(int level, const char *message_fmt, ...);
+extern void daemon_printf(const char *message_fmt, ...)
+    __attribute__((format(printf, 1, 2)));
+extern void daemon_printf_level(int level, const char *message_fmt, ...)
+    __attribute__((format(printf, 2, 3)));
 extern void daemon_perror(const char *message);
 
 #endif

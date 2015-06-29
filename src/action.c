692eedfe6f5a812e5f73224293456550e8c366fb
Felix Palmen <felix@palmen-it.de>
2015-06-29 10:01:40 +0200

    pull in libpcre, compile patterns

--- src/action.c
+++ src/action.c
@@ -1,10 +1,14 @@
 #include "action.h"
 
+#include <pcre.h>
+
+#include "daemon.h"
 #include "util.h"
 
 struct action
 {
     const CfgAct *cfgAct;
+    pcre *re;
     Action *next;
 };
 
@@ -26,10 +30,23 @@ action_append(Action *self, Action *act)
 Action *
 action_appendNew(Action *self, const CfgAct *cfgAct)
 {
-    Action *next = lladAlloc(sizeof(Action));
+    Action *next;
+    pcre *re;
+    const char *error;
+    int erroffset;
 
+    re = pcre_compile(cfgAct_pattern(cfgAct), 0, &error, &erroffset, NULL);
+    if (!re)
+    {
+	daemon_printf_level(LEVEL_WARNING,
+		"Action `%s' error in pattern: %s", cfgAct_name(cfgAct), error);
+	return NULL;
+    }
+
+    next = lladAlloc(sizeof(Action));
     next->next = NULL;
     next->cfgAct = cfgAct;
+    next->re = re;
 
     return action_append(self, next);
 }
@@ -51,6 +68,7 @@ action_free(Action *self)
     {
 	last = curr;
 	curr = last->next;
+	pcre_free(last->re);
 	free(last);
     }
 }

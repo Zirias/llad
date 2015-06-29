e9dbdd555531f5f8ec78c89536765d9507194de4
Felix Palmen <felix@palmen-it.de>
2015-06-29 14:49:09 +0200

    create thread for executing action
    so output of child process can be captured while still coninuing to wait
    for log lines.

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CFLAGS = -O3 -Wall -Werror -pedantic -std=c99
 
 llad_OBJS := obj/llad.o obj/util.o obj/daemon.o obj/config.o obj/logfile.o \
     obj/watcher.o obj/action.o
-llad_LIBS := -lpopt -lpcre
+llad_LIBS := -pthread -lpopt -lpcre
 
 llad: $(llad_OBJS)
 	$(CC) -o $@ $^ $(llad_LIBS)
--- src/action.c
+++ src/action.c
@@ -1,10 +1,15 @@
+#define _POSIX_C_SOURCE 200809L
 #include "action.h"
 
+#include <pthread.h>
+#include <signal.h>
 #include <string.h>
 #include <pcre.h>
 
 #include "daemon.h"
 #include "util.h"
+
+#define CMDPATH "/etc/llad/command/"
 
 struct action
 {
@@ -15,6 +20,27 @@ struct action
     int ovecsize;
     int ovec[];
 };
+
+struct actionExecArgs
+{
+    const char *logname;
+    const char *actname;
+    char **cmd;
+};
+
+static char *cmdpath = CMDPATH;
+
+static int siginitialized = 0;
+static void
+initsig(void)
+{
+    struct sigaction handler;
+    memset(&handler, 0, sizeof(handler));
+    handler.sa_handler = SIG_IGN;
+    sigemptyset(&(handler.sa_mask));
+    sigaction(SIGCHLD, &handler, NULL);
+    siginitialized = 1;
+}
 
 Action *
 action_append(Action *self, Action *act)
@@ -65,10 +91,70 @@ action_appendNew(Action *self, const CfgAct *cfgAct)
     return action_append(self, next);
 }
 
+struct actionExecArgs *
+createExecArgs(const Action *self, const char *logname,
+	const char *line, int numArgs)
+{
+    char *cmdName;
+    char *arg;
+    int i;
+    size_t captureLength;
+
+    struct actionExecArgs *args = lladAlloc(sizeof(struct actionExecArgs));
+    args->logname = logname;
+    args->actname = cfgAct_name(self->cfgAct);
+    args->cmd = lladAlloc((numArgs + 2) * sizeof(char *));
+
+    cmdName = lladAlloc(strlen(cmdpath) +
+	    strlen(cfgAct_command(self->cfgAct)) + 1);
+    strcpy(cmdName, cmdpath);
+    strcat(cmdName, cfgAct_command(self->cfgAct));
+    args->cmd[0] = cmdName;
+
+    for (i = 0; i < numArgs; ++i)
+    {
+	captureLength = self->ovec[2*i+1] - self->ovec[2*i];
+	arg = lladAlloc(captureLength + 1);
+	arg[captureLength] = '\0';
+	strncpy(arg, line + self->ovec[2*i], captureLength);
+	args->cmd[i+1] = arg;
+    }
+    args->cmd[numArgs+1] = NULL;
+
+    return args;
+}
+
+void
+freeExecArgs(struct actionExecArgs *args)
+{
+    char **argptr = args->cmd;
+    while (*argptr)
+    {
+	free (*argptr);
+	++argptr;
+    }
+    free(args);
+}
+
+void *
+actionExec(void *argsPtr)
+{
+    struct actionExecArgs *args = argsPtr;
+
+    daemon_printf_level(LEVEL_DEBUG,
+	    "[%s]: Thread for action `%s' started.",
+	    args->logname, args->actname);
+
+    freeExecArgs(args);
+    return NULL;
+}
+
 int
 action_matchAndExecChain(Action *self, const char *logname, const char *line)
 {
     int rc;
+    pthread_attr_t attr;
+    pthread_t thread;
 
     while (self)
     {
@@ -79,6 +165,22 @@ action_matchAndExecChain(Action *self, const char *logname, const char *line)
 	    daemon_printf("[%s]: Action `%s' matched, executing `%s'.",
 		    logname, cfgAct_name(self->cfgAct),
 		    cfgAct_command(self->cfgAct));
+
+	    struct actionExecArgs *args = createExecArgs(
+		    self, logname, line, rc);
+
+	    if (!siginitialized) initsig();
+
+	    if (pthread_attr_init(&attr) != 0
+		    || pthread_attr_setdetachstate(&attr,
+			PTHREAD_CREATE_DETACHED) != 0
+		    || pthread_create(&thread, &attr, &actionExec, args) != 0)
+	    {
+		daemon_printf_level(LEVEL_WARNING,
+			"[%s]: Unable to create thread for action `%s', "
+			"giving up.", logname, cfgAct_name(self->cfgAct));
+		freeExecArgs(args);
+	    }
 	}
 	self = self->next;
     }

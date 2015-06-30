ccc17e0139f0bea8de04036f282791863c0190d1
Felix Palmen <felix@palmen-it.de>
2015-06-30 23:49:18 +0200

    removed unnecessary "extern" decls

--- src/daemon.h
+++ src/daemon.h
@@ -5,7 +5,7 @@
 
 typedef int (*daemon_loop)(void *data);
 
-extern const struct poptOption daemon_opts[];
+const struct poptOption daemon_opts[];
 #define DAEMON_OPTS {NULL, '\0', POPT_ARG_INCLUDE_TABLE, (struct poptOption *)daemon_opts, 0, "Daemon options:", NULL},
 
 enum daemon_levels
@@ -20,15 +20,15 @@ enum daemon_levels
     LEVEL_EMERG
 };
 
-extern void daemon_init(const char *name);
-extern int daemon_daemonize(const daemon_loop daemon_main, void *data);
-extern void daemon_print(const char *message);
-extern void daemon_print_level(int level, const char *message);
-extern void daemon_printf(const char *message_fmt, ...)
+void daemon_init(const char *name);
+int daemon_daemonize(const daemon_loop daemon_main, void *data);
+void daemon_print(const char *message);
+void daemon_print_level(int level, const char *message);
+void daemon_printf(const char *message_fmt, ...)
     __attribute__((format(printf, 1, 2)));
-extern void daemon_printf_level(int level, const char *message_fmt, ...)
+void daemon_printf_level(int level, const char *message_fmt, ...)
     __attribute__((format(printf, 2, 3)));
-extern void daemon_perror(const char *message);
+void daemon_perror(const char *message);
 
 #endif
 
